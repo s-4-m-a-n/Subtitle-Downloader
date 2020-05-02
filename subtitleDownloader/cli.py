d3405b8288bd7daf0326060537472d5904854429
suman <dhakalsumn739@gmail.com>
2020-05-02 18:50:49 +0545

    year default value is added

--- subtitleDownloader/cli.py
+++ subtitleDownloader/cli.py
@@ -9,7 +9,7 @@ def main():
 	parser.add_argument("-v","--version",help="display version",action="store_true")
 
 	parser.add_argument("-t","--title",default=None,type=str.lower,help="enter the movie title")
-	parser.add_argument("-y","--year",type=str,help="enter the released year")
+	parser.add_argument("-y","--year",default=' ',type=str,help="enter the released year")
 	parser.add_argument("-lng","--language",type=str.lower,default="english",choices=["english","arabic","albanian","arabic","bengali","turkish","spanish","swedish","thai","slovenian","russian","portuguese","malay","farsi/persian"],help="enter any language from choice, default value='english' " )
 	parser.add_argument("-l","--limit",type=str.lower,default="1",help="No of files you want to download , default value = 1")
 	args = parser.parse_args()
@@ -24,7 +24,7 @@ def main():
 		print("{}".format(info))
 
 
-		downloader.main(title=args.title,year=args.year,language=args.language,limit=arg.limit)
+		downloader.main(title=args.title,year=args.year,language=args.language,limit=args.limit)
 
 if __name__ == "__main__":
 	main()
